5b59f600b7406ad0151c0f195ce1259f7177af01
Tara Sabella <tarasabella@gmail.com>
2018-11-20 17:52:11 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,7 @@
-
 def square_array(array)
   squared = []
   array.each { |element| squared << element ** 2 }
   squared
 end
+
+# calls on each and squares each element in original array which is stored in new array 
