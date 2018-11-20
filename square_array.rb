858b2c2344e78932bb3968e50c64d1f416f9f917
Tara Sabella <tarasabella@gmail.com>
2018-11-20 17:51:08 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
-def square_array(array)
-  squared_array = []
-  array.each { |element| squared << element ** 2 }
-  squared_array
-end
 
+def square_array(array)
+  squared = []
+  array.each { |element| squared << element ** 2 }
+  squared
+end
